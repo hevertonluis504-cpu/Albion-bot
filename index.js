80327c5ea0f0b2e2babdf50635e4ef1ad8d7bde7
hevertonluis504-cpu <hevertonluis.504@gmail.com>
2026-02-24 20:55:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -109,3 +109,41 @@ client.on("interactionCreate", async (interaction) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+client.once('clientReady', async () => {const { SlashCommandBuilder, REST, Routes } = require("discord.js");
+
+const commands = [
+
+  new SlashCommandBuilder()
+    .setName("criar")
+    .setDescription("Criar grupo de conteúdo")
+    .addStringOption(o=>o.setName("tipo").setDescription("Tipo do conteúdo").setRequired(true))
+    .addIntegerOption(o=>o.setName("jogadores").setDescription("Total de jogadores").setRequired(true))
+    .addStringOption(o=>o.setName("classes").setDescription("Ex: 1 Tank, 2 Healer, 3 DPS").setRequired(true))
+    .addStringOption(o=>o.setName("data").setDescription("DD/MM/AAAA").setRequired(true))
+    .addStringOption(o=>o.setName("horario").setDescription("HH:MM UTC-3").setRequired(true))
+    .addStringOption(o=>o.setName("descricao").setDescription("Descrição")),
+
+  new SlashCommandBuilder()
+    .setName("divisao")
+    .setDescription("Dividir valor entre jogadores")
+    .addIntegerOption(o=>o.setName("valor").setDescription("Valor total").setRequired(true))
+    .addIntegerOption(o=>o.setName("pessoas").setDescription("Quantidade de pessoas").setRequired(true)),
+
+  new SlashCommandBuilder()
+    .setName("tocar")
+    .setDescription("Tocar música no canal de voz")
+    .addStringOption(o=>o.setName("musica").setDescription("Nome ou link da música").setRequired(true))
+
+].map(c=>c.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+const app = await rest.get(Routes.oauth2CurrentApplication());
+
+await rest.put(
+  Routes.applicationCommands(app.id),
+  { body: commands }
+);
+
+console.log("Comandos registrados automaticamente!");
